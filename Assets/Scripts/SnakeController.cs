8bbdc71d84959f87deda34c624b4fce5fe9d54e0
unqnq <140976693+unqnq@users.noreply.github.com>
2025-08-12 21:30:52 +0300

    Move snake

--- Assets/Scripts/SnakeController.cs
+++ Assets/Scripts/SnakeController.cs
@@ -1,14 +1,15 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SnakeController : MonoBehaviour
 {
     [SerializeField] private GameObject foodPrefab, tailPrefab;
     private GameObject food;
-    [SerializeField] private float stepRate, currentAngleZ;
-    private Vector2 move;
+    [SerializeField] private float stepRate = 0.3f, currentDirection = 0.0f;
+    private Vector2 move = new Vector2(-1f, 0);
     private List<Transform> tail = new List<Transform>();
-    private bool issFoodEating;
+    private bool issFoodEating = false;
     private Vector2 topWallPosition, bottomWallPosition, leftWallPosition, rightWallPosition;
 
     void Start()
@@ -17,6 +18,7 @@ public class SnakeController : MonoBehaviour
         bottomWallPosition = GameObject.Find("BottomWall").transform.position;
         leftWallPosition = GameObject.Find("LeftWall").transform.position;
         rightWallPosition = GameObject.Find("RightWall").transform.position;
+
         InvokeRepeating("Movement", 0.1f, stepRate);
         SpawnFood();
     }
@@ -30,6 +32,16 @@ public class SnakeController : MonoBehaviour
 
     void Movement()
     {
+        Vector2 currentPosition = transform.position;
+        transform.position = currentPosition + move;
+    }
 
+    public void SetDirection(InputAction.CallbackContext context)
+    {
+        Vector2 newDirection = context.ReadValue<Vector2>();
+        if (newDirection == Vector2.zero) return;
+        if (newDirection == -move) return;
+        if (Mathf.Abs(newDirection.x) > 0 && Mathf.Abs(newDirection.y) > 0) return; // ігнорувати діагональ 
+        move = newDirection;
     }
 }
